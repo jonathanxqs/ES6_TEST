36bbcd05176c1c1b3cef332f156856bd2e48d6d9
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-25 16:03:02 -0400

    'Test hoisting

---
+++ hoist.js
@@ -0,0 +1,14 @@
+// One of the advantages you get from JavaScript putting function 
+// declarations into memory first, before it executes any coding, 
+// is this allows you to use a function before you declare it in your coding. 
+
+catName("Chloe");
+
+function catName(name) {
+  console.log("My cats name is " + name);
+}
+/*
+The results of the coding above is: "My cats name is Chloe"
+*/
+
+
