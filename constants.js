9dbfdf9a6de23c068bed694a03f6b0621847e9c2
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-26 23:48:49 -0400

    const immutable

--- constants.js
+++ constants.js
@@ -1,8 +1,13 @@
 const PI = 3.141593
 PI > 3.0
+console.log(PI)
 
 
-//  only in ES5 through the help of object properties
+PI = 3.5
+console.log(PI)
+
+
+/*//  only in ES5 through the help of object properties
 //  and only in global context and not in a block scope
 Object.defineProperty(typeof global === "object" ? global : window, "PI", {
     value:        3.141593,
@@ -10,4 +15,4 @@ Object.defineProperty(typeof global === "object" ? global : window, "PI", {
     writable:     false,
     configurable: false
 })
-PI > 3.0;
+PI > 3.0;*/
