0cfdbdeccdfc6614407318626150ca3db215210c
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-27 18:36:17 -0400

    difference btw es5 and se6

--- block-scoped-variables.js
+++ block-scoped-variables.js
@@ -1,3 +1,7 @@
+/*Scoping
+Block-Scoped Variables
+Block-scoped variables (and constants) without hoisting.
+*/
 for (let i = 0; i < a.length; i++) {
     let x = a[i]
     …
@@ -14,3 +18,26 @@ for (let i = 0; i <= 2; i++) {
 callbacks[0]() === 0
 callbacks[1]() === 2
 callbacks[2]() === 4
+
+
+
+// ECMAScript 5
+var i, x, y;
+for (i = 0; i < a.length; i++) {
+    x = a[i];
+    …
+}
+for (i = 0; i < b.length; i++) {
+    y = b[i];
+    …
+}
+
+var callbacks = [];
+for (var i = 0; i <= 2; i++) {
+    (function (i) {
+        callbacks[i] = function() { return i * 2; };
+    })(i);
+}
+callbacks[0]() === 0;
+callbacks[1]() === 2;
+callbacks[2]() === 4;   
