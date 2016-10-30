0741a86f0e145ca3279edd985ad6ce961ec8eda3
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-29 23:03:40 -0400

    Arrow func

--- Arrow_function.js
+++ Arrow_function.js
@@ -1,7 +1,16 @@
-evens=[0,2,4,6,8]
+evens=[0,2,4,6,8,10,12]
 
 odds  = evens.map(v => v + 1)
 pairs = evens.map(v => ({ even: v, odd: v + 1 }))
-nums  = evens.map((v, i) => v + i)
+nums  = evens.map((v, j) => v + j)
 
-console.log(odds);
+console.log(nums);
+
+let fives=[]
+
+nums.forEach(v => {
+   if (v % 5 === 0)
+       fives.push(v)
+})
+
+console.log(fives);
