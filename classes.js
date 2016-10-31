ca92bce601bf4a7e7d13aafd6579cfe6d241360d
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-30 22:00:10 -0400

    Class

---
+++ classes.js
@@ -0,0 +1,23 @@
+class SkinnedMesh extends THREE.Mesh {//使用extends来表示继承
+  constructor(geometry, materials) {
+    super(geometry, materials);//父类的构造函数
+
+    this.idMatrix = SkinnedMesh.defaultMatrix();
+    this.bones = [];
+    this.boneMatrices = [];
+    //...
+  }
+  update(camera) {
+    //...
+    super.update();
+  }
+  get boneCount() {
+    return this.bones.length;
+  }
+  set matrixType(matrixType) {
+    this.idMatrix = SkinnedMesh[matrixType]();
+  }
+  static defaultMatrix() {//静态方法
+    return new THREE.Matrix4();
+  }
+}
