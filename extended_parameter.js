fb4b6165b461aa2af549a6ec452341e0b17be461
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-31 22:38:14 -0400

    rest

--- extended_parameter.js
+++ extended_parameter.js
@@ -1,4 +1,19 @@
+/* 
+    Default Parameter Values
+    Simple and intuitive default values for function parameters.
+*/
+
 function f (x, y = 7, z = 42) {
     return x + y + z
 }
 f(1) === 50
+
+
+/* 
+Rest Parameter
+Aggregation of remaining arguments into single parameter of variadic functions.
+*/
+function f (x, y, ...a) {
+    return (x + y) * a.length
+}
+f(1, 2, "hello", true, 7) === 9
