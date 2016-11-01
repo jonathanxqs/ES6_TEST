e2159515cef1942c551cf13bbc3cc4dc477e9144
Rhapsodistic <xucanxucan123@gmail.com>
2016-10-31 22:40:08 -0400

    log

--- extended_parameter.js
+++ extended_parameter.js
@@ -14,6 +14,9 @@ Rest Parameter
 Aggregation of remaining arguments into single parameter of variadic functions.
 */
 function f (x, y, ...a) {
-    return (x + y) * a.length
+    console.log(a[0]);
+    console.log(a);
+    return (x + y) * a.length;
 }
-f(1, 2, "hello", true, 7) === 9
+
+console.log( f(1, 2, "hello", true, 7) )  // 9
