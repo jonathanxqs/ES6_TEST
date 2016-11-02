550a90b8e6d35c862ae44c5d62ceb36514324940
Rhapsodistic <xucanxucan123@gmail.com>
2016-11-01 23:51:08 -0400

    Spread

--- extended_parameter.js
+++ extended_parameter.js
@@ -20,3 +20,13 @@ function f (x, y, ...a) {
 }
 
 console.log( f(1, 2, "hello", true, 7) )  // 9
+
+
+
+
+var params = [ "hello", true, 7 ]
+var other = [ 1, 2, ...params ] // [ 1, 2, "hello", true, 7 ]
+f(1, 2, ...params) === 9
+
+var str = "foo"
+var chars = [ ...str ] // [ "f", "o", "o" ]
