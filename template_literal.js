7541f61eb4a3eda5b17f47eb3e84c580f3824798
Rhapsodistic <xucanxucan123@gmail.com>
2016-11-02 18:17:50 -0400

    smart $

--- template_literal.js
+++ template_literal.js
@@ -25,3 +25,16 @@ POST`http://foo.org/bar?a=${a}&b=${b}
      X-Credentials: ${credentials}
      { "foo": ${foo},
        "bar": ${bar}}`(myOnReadyStateChangeHandler);
+
+/*
+Intuitive expression interpolation for single-line and multi-line strings. (Notice: don't be confused, Template Literals were originally named "Template Strings" in the drafts of the ECMAScript 6 language specification)
+
+*/  
+
+var customer = { name: "Foo" }
+var card = { amount: 7, product: "Bar", unitprice: 42 }
+var message = `Hello ${customer.name},
+want to buy ${card.amount} ${card.product} for
+a total of ${card.amount * card.unitprice} bucks?`
+
+  
