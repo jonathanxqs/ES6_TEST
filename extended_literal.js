fa5663a1c20f11504886b4c79f95fba80036e25b
Rhapsodistic <xucanxucan123@gmail.com>
2016-11-09 00:33:19 -0500

    unicode

--- extended_literal.js
+++ extended_literal.js
@@ -1,5 +1,23 @@
 
+/*
+Extended Literals
+Binary & Octal Literal
+Direct support for safe binary and octal literals.
 
+*/
 
 0b111110111 === 503
 0o767 === 503
+
+/*
+Unicode String & RegExp Literal
+Extended support using Unicode within strings and regular expressions.
+
+*/
+
+"𠮷".length === 2
+"𠮷".match(/./u)[0].length === 2
+"𠮷" === "\uD842\uDFB7"
+"𠮷" === "\u{20BB7}"
+"𠮷".codePointAt(0) == 0x20BB7
+for (let codepoint of "𠮷") console.log(codepoint)
