f8798aaf8574be959323a8b5a82e445a84c50561
Rhapsodistic <xucanxucan123@gmail.com>
2017-08-18 11:50:37 +0800

    replace var

--- con_let_var.js
+++ con_let_var.js
@@ -7,11 +7,13 @@ PI > 3.0
 console.log(PI)
 
 
-PI = 3.5
-console.log(PI)
+//PI = 3.5
+//console.log(PI)
 
 
-/*//  only in ES5 through the help of object properties
+/*
+
+//  only in ES5 through the help of object properties
 //  and only in global context and not in a block scope
 Object.defineProperty(typeof global === "object" ? global : window, "PI", {
     value:        3.141593,
@@ -19,7 +21,9 @@ Object.defineProperty(typeof global === "object" ? global : window, "PI", {
     writable:     false,
     configurable: false
 })
-PI > 3.0;*/
+PI > 3.0;
+
+*/
 
 
 
@@ -29,16 +33,17 @@ const a=1;
 // a=2;
 // error:Assignment to constant variable.
 // 【const是常数变量，即一般在require一个模块的时候用或者定义一些全局常量，一旦定义，无法更改，无法重复赋值】
-document.write('a: '+a+'<br />');
+console.log('a: '+a);
 
 let b=1;
 // let a=2; 
 // error:Identifier 'a' has already been declared
 // 【let 变量，块作用域，不能重复声明覆盖，限制了变量的作用域，保证变量不会去污染全局变量，所以尽量将var改为用let。】
 b=2;
-document.write('b: '+b+'<br />');
+console.log('b: '+b);
 
 var c=1;
 var c=2;
+console.log('c: '+c);
 c=3;
-document.write('c: '+c);
+console.log('c: '+c);
