c33cab501871fa04dbc297abf7b73da63b72e049
Rhapsodistic <xucanxucan123@gmail.com>
2017-08-27 12:18:19 +0800

    console .log or assert

--- block_scoped_func.js
+++ block_scoped_func.js
@@ -1,11 +1,12 @@
 
 {
     function foo () { return 1 }
-    assert(foo() === 1 , ' =1')
+    console.assert(foo() === 1 , ' =1')
     {
         function foo () { return 2 }
-        assert ( foo() === 2 ,' =2'); 
+        console.log ( foo() === 1 ? "" :  "inner block foo != 1" ); 
     }
-    assert(foo() === 1 , ' =1')
+    console.assert(foo() === 1 , ' =1')
 }
 
+// console.dir(console);
