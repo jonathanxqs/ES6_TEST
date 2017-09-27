422964ef02528b36942783c775e269de5370b2fc
Rhapsodistic <xucanxucan123@gmail.com>
2017-09-27 00:13:04 -0400

    rest

--- extended_parameter.js
+++ extended_parameter.js
@@ -33,3 +33,24 @@ f(1, 2, ...params) === 9
 
 var str = "foo"
 var chars = [ ...str ] // [ "f", "o", "o" ]
+
+
+
+function add(...x){
+    return x.reduce((m,n)=>m+n);
+}
+
+console.log(add(1,2,3));//输出：6
+console.log(add(1,2,3,4,5));//输出：15
+
+
+var people=['Wayou','John','Sherlock'];
+//sayHello函数本来接收三个单独的参数人妖，人二和人三
+function sayHello(people1,people2,people3){
+    console.log(`Hello ${people1},${people2},${people3}`);
+}
+//但是我们将一个数组以拓展参数的形式传递，它能很好地映射到每个单独的参数
+sayHello(...people);//输出：Hello Wayou,John,Sherlock 
+
+//而在以前，如果需要传递数组当参数，我们需要使用函数的apply方法
+sayHello.apply(null,people);//输出：Hello Wayou,John,Sherlock 
